8d2f1911b98d4f59fa4e2e33f225eae5b677af31
hwborchers <hwborchers@googlemail.com>
2023-10-20 19:33:27 +0200

    Corrected and extended Historize().

--- Historize.R
+++ Historize.R
@@ -3,56 +3,46 @@
 ### Definition
 #
 #  Add history storage to a function
-#  histfun() will return the storage
+#  Histfun() will return the storage
 #
 
-historize <- function(fun, len = 0) {
-    if (!is.function(fun))
-        stop("'fun' must be a (numeric) function.")
-    if (len != floor(len) || len < 0)
-        stop("'len' must be a non-negative integer.")
+Historize <- function (fun, len = 0, ...) {
+    stopifnot(is.function(fun))
+    if (len != floor(len) || len < 0) 
+        stop("Argument 'len' must be a non-negative integer.")
     local({
-        H <- numeric()
-        myFun <- function(x) {
-            if(missing(x)) {
-                return(H)
-            } else if (is.null(x)) {
-                H <<- numeric()
-                return(invisible(NULL))
-            } else {
-                y <- fun(x)     # storing parameters is slow
-                if (len == 0) {
-                    H <<- c(H, y)
-                } else
-                    H <<- c(H, x, y)    # or: rbind(H, c(x, y))
-                return(y)
-            }
-        }
-        return(myFun)
-    })
-}
-
-Historize <- function(fun, len = 0, ...) {
-    if (!is.function(fun))
-        stop("'fun' must be a (numeric) function.")
-    if (len != floor(len) || len < 0)
-        stop("'len' must be a non-negative integer.")
-    local({
-        H <- numeric()
+        H1 <- H2 <- numeric()
         myFun <- function(x, ...) {
-            if(missing(x)) {
-                return(H)
-            } else if (is.null(x)) {
-                H <<- numeric()
+            if (missing(x)) {           # return storage
+                if (len == 0) {
+                    m1 <- 0; m2 <- length(H2)
+                    return(list(input=H1, values=H2, nvars = 1, ncalls = m2))
+                } else {
+                    m1 <- length(H1); m2 <- length(H2)
+                    if (m2 == 0) {
+                        return(list(input = NULL, values = NULL,
+                                    nvars = 0, ncalls = 0))
+                    } else if (m1 %% m2 != 0) {
+                        stop("Input length not consistent with no. of calls.")
+                    } else {
+                        n <- m1/m2
+                    }
+                    H1m <- matrix(H1, ncol = n, byrow = TRUE)
+                    return(list(input=H1m, values=H2, nvars = n, ncalls = m2))
+                }
+            }
+            else if (is.null(x)) {      # clear storage
+                H1 <<- H2 <<- numeric()
                 return(invisible(NULL))
-            } else {
+            }
+            else {
                 y <- fun(x, ...)
                 if (len == 0) {
-                    H <<- c(H, y)
-                } else {
-                    if (length(x) != len)
-                        stop("Incorrect parameter length.")
-                    H <<- rbind(H, c(x, y))
+                    H2 <<- c(H2, y)     # function values
+                }
+                else {
+                    H1 <<- c(H1, x)     # input values
+                    H2 <<- c(H2, y)     # function values
                 }
                 return(y)
             }
@@ -68,30 +58,20 @@ Historize <- function(fun, len = 0, ...) {
 #
 dm <- 10
 lb <- rep(-5.2, 10); ub <- -lb
-Fn1 <- historize(adagio::fnRastrigin, len = dm)
-Fn2 <- Historize(adagio::fnRastrigin, len = dm)
+Fn0 <- Historize(adagio::fnRastrigin, len = 0)
+Fn1 <- Historize(adagio::fnRastrigin, len = 1)
 
-#-- Fn1(NULL)
+#-- Store values only
 system.time(
-    sol <- DEoptimR::JDEoptim(lb, ub, Fn1)  # 36 sec, 47000 evals
-)  # 40 sec, 490000 evals
-H <- matrix(Fn1(), ncol = dm+1, byrow = TRUE)
-fvalues <- H[, dm+1]
+    sol <- DEoptimR::JDEoptim(lb, ub, Fn0)  # 36 sec, 47000 evals
+)  # 4.8 sec
+fvalues <- Fn0()$values
 plot(fvalues, type = 'p', col = "navy", pch = '.', cex = 1.5)
 
-#-- Fn2(NULL)
+#-- Store input and values
 system.time(
-    sol <- DEoptimR::JDEoptim(lb, ub, Fn2)  # 32 sec, 46000 evals
+    sol <- DEoptimR::JDEoptim(lb, ub, Fn1)  # 30.5 sec
 )
-H <- Fn2()
-fvalues <- H[, dm+1]
-plot(fvalues, type = 'p', col = "darkred", pch = '.', cex = 1.5)
-
-#-- Fn(NULL)
-Fn <- Historize(adagio::fnRastrigin)
-system.time(
-    sol <- DEoptimR::JDEoptim(lb, ub, Fn)  # 4.8 sec. 46000 evals
-)
-fvalues <- Fn()
-plot(fvalues, type = 'p', col = "navy", pch = '.', cex = 1.5)
+fvalues <- Fn1()$values
+points(fvalues, type = 'p', col = "darkred", pch = '.', cex = 1.5)
 
